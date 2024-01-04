309e6adde21a204f5916adff8f9d5f37d4917e90
xPolar <polar@polar.blue>
2024-01-04 12:45:16 -0800

    Track approximate user count more accurately, data -> guild in guild events.

--- src/bot/events/guildCreate.ts
+++ src/bot/events/guildCreate.ts
@@ -13,35 +13,36 @@ export default class GuildCreate extends EventHandler {
 	 *
 	 * https://discord.com/developers/docs/topics/gateway-events#guild-create
 	 */
-	public override async run({ shardId, data }: WithIntrinsicProps<GatewayGuildCreateDispatchData>) {
+	public override async run({ shardId, data: guild }: WithIntrinsicProps<GatewayGuildCreateDispatchData>) {
 		const guildRoles = new Map();
 
-		for (const guildRole of data.roles) guildRoles.set(guildRole.id, guildRole);
+		for (const guildRole of guild.roles) guildRoles.set(guildRole.id, guildRole);
 
-		this.client.guildRolesCache.set(data.id, guildRoles);
-		if (this.client.guildOwnersCache.get(data.id) === undefined) {
+		this.client.guildRolesCache.set(guild.id, guildRoles);
+		this.client.approximateUserCount += guild.member_count;
+
+		if (this.client.guildOwnersCache.get(guild.id) === undefined) {
 			this.client.dataDog.increment("guild_count", 1, [`shard:${shardId}`]);
 
-			this.client.guildOwnersCache.set(data.id, data.owner_id);
-			this.client.approximateUserCount += data.member_count;
+			this.client.guildOwnersCache.set(guild.id, guild.owner_id);
 
 			this.client.logger.info(
-				`Joined ${data.name} [${data.id}] with ${data.member_count} members on Shard ${shardId}. Now at ${this.client.guildOwnersCache.size} guilds with ${this.client.approximateUserCount} total users.`,
+				`Joined ${guild.name} [${guild.id}] with ${guild.member_count} members on Shard ${shardId}. Now at ${this.client.guildOwnersCache.size} guilds with ${this.client.approximateUserCount} total users.`,
 			);
 
 			return this.client.logger.webhookLog("guild", {
 				content: `**__Joined a New Guild (${this.client.guildOwnersCache.size} Total)__**\n**Guild Name:** \`${
-					data.name
-				}\`\n**Guild ID:** \`${data.id}\`\n**Guild Owner:** <@${data.owner_id}> \`[${
-					data.owner_id
+					guild.name
+				}\`\n**Guild ID:** \`${guild.id}\`\n**Guild Owner:** <@${guild.owner_id}> \`[${
+					guild.owner_id
 				}]\`\n**Guild Member Count:** \`${
-					data.member_count
+					guild.member_count
 				}\`\n**Timestamp:** ${this.client.functions.generateTimestamp()}\n**Shard ID:** \`${shardId}\``,
 				username: `${this.client.config.botName} | Console Logs`,
 				allowed_mentions: { parse: [] },
 			});
-		} else if (this.client.guildOwnersCache.get(data.id) !== data.owner_id)
-			return this.client.guildOwnersCache.set(data.id, data.owner_id);
+		} else if (this.client.guildOwnersCache.get(guild.id) !== guild.owner_id)
+			return this.client.guildOwnersCache.set(guild.id, guild.owner_id);
 
 		return true;
 	}
--- src/bot/events/guildDelete.ts
+++ src/bot/events/guildDelete.ts
@@ -13,21 +13,21 @@ export default class GuildDelete extends EventHandler {
 	 *
 	 * https://discord.com/developers/docs/topics/gateway-events#guild-delete
 	 */
-	public override async run({ shardId, data }: WithIntrinsicProps<GatewayGuildDeleteDispatchData>) {
-		if (data.unavailable) return;
+	public override async run({ shardId, data: guild }: WithIntrinsicProps<GatewayGuildDeleteDispatchData>) {
+		if (guild.unavailable) return;
 
 		this.client.dataDog.increment("guild_count", -1, [`shard:${shardId}`]);
 
-		this.client.guildRolesCache.delete(data.id);
-		this.client.guildOwnersCache.delete(data.id);
+		this.client.guildRolesCache.delete(guild.id);
+		this.client.guildOwnersCache.delete(guild.id);
 
 		this.client.logger.info(
-			`Left guild ${data.id} on Shard ${shardId}. Now at ${this.client.guildOwnersCache.size} guilds with ${this.client.approximateUserCount} total users.`,
+			`Left guild ${guild.id} on Shard ${shardId}. Now at ${this.client.guildOwnersCache.size} guilds with ${this.client.approximateUserCount} total users.`,
 		);
 
 		return this.client.logger.webhookLog("guild", {
 			content: `**__Left a Guild (${this.client.guildOwnersCache.size} Total)__**\n**Guild ID:** \`${
-				data.id
+				guild.id
 			}\`\n**Timestamp:** ${this.client.functions.generateTimestamp()}\n**Shard ID:** \`${shardId}\``,
 			username: `${this.client.config.botName} | Console Logs`,
 			allowed_mentions: { parse: [] },
